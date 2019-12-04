e711f6f6fcc63168d1ae4fcc3ad35af8e3161ac6
yh06100 <e_yunho@naver.com>
2019-12-04 22:14:32 +0900

    Event master

--- OYH.CS/Form1.cs
+++ OYH.CS/Form1.cs
@@ -15,6 +15,26 @@ namespace OYH.CS
         public Form1()
         {
             InitializeComponent();
+
+            button1.Click += Button1_Click;
+            textBox1.KeyPress += TextBox1_KeyPress;
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Test();
+        }
+
+
+
+        public void Test()
+        {
+            MessageBox.Show("Test");
         }
     }
 }
