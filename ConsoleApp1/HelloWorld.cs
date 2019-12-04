050c0f068a171e513dc4195183def59c7dea58c0
yh06100 <e_yunho@naver.com>
2019-12-04 21:53:25 +0900

    practiceYH

---
+++ ConsoleApp1/HelloWorld.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //int x = 1;
+            //int y = 1;
+            //int i = 0;
+            //for(int j = 0; j < 9; j++)
+            //{
+            //    if(i<9)
+            //    {
+            //        for (i = 0; i < 9; i++)
+            //        {
+            //            int z = x * y;
+            //            Console.WriteLine(x + "*" + y + "=" + z);
+
+            //            y++;
+            //        }
+            //    }
+            //    x++;
+            //}
+            //Console.ReadKey();
+
+
+            for (int y = 1; y < 10; y++)
+            {
+                Console.WriteLine(2 + "* " + y + "=" + 2 * y);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
