fd6b2318eb89d440ff651a9a2c9ece1700843202
yh06100 <e_yunho@naver.com>
2019-12-20 22:30:51 +0900

    MybatisNET Setting

--- OYH.CS/Chapter1/P006.cs
+++ OYH.CS/Chapter1/P006.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IBatisNet.DataMapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OYH.CS.MybatisNET;
+using OYH.CS.MybatisNET.VO;
 
 namespace OYH.CS.Chapter1
 {
@@ -23,13 +26,86 @@ namespace OYH.CS.Chapter1
         {
             try
             {
-                MessageBox.Show(tb_NUMBER.Text);
+                //MessageBox.Show(tb_NUMBER.Text);
+                SelectSTUDENT();
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                MessageBox.Show(ex.Message);
             }
         }
+
+        public static void SelectSTUDENT()
+        {
+            ISqlMapper mapper = EntityMapper;
+            StudentVO testVo = new StudentVO();
+            IList<StudentVO> resultList = mapper.QueryForList<StudentVO>("SelectSTUDENT", testVo);
+
+            for (int x = 0; x < resultList.Count; x++)
+            {
+                Console.WriteLine(resultList[x].NUMBER + resultList[x].NAME + resultList[x].AGE + resultList[x].SEX);
+            }
+        }
+        public static void InsertSTUDENT()
+        {
+            
+            ISqlMapper mapper = EntityMapper;
+            StudentVO testVo = new StudentVO() { NUMBER = "13-71019424", NAME = "OHYUNHO", AGE = "27", SEX = "MAN" };
+            mapper.Insert("InsertSTUDENT", testVo);
+        }
+        public static void SelectSCORE()
+        {
+            ISqlMapper mapper = EntityMapper;
+            StudentVO testVo = new StudentVO();
+            IList<StudentVO> resultList = mapper.QueryForList<StudentVO>("SelectSCORE", testVo);
+
+            for (int x = 0; x < resultList.Count; x++)
+            {
+                Console.WriteLine(resultList[x].NUMBER + resultList[x].KOREAN + resultList[x].ENGLISH + resultList[x].MATH
+                    + resultList[x].SOCIAL + resultList[x].SCIENCE);
+            }
+        }
+        public static void InsertSCORE()
+        {
+            ISqlMapper mapper = EntityMapper;
+            StudentVO testVo = new StudentVO()
+            {
+                NUMBER = "13-71019424",
+                KOREAN = "10",
+                ENGLISH = "20",
+                MATH = "30"
+            ,
+                SOCIAL = "40",
+                SCIENCE = "50"
+            };
+            mapper.Insert("InsertSCORE", testVo);
+        }
+
+
+        // 
+        public static ISqlMapper EntityMapper
+        {
+            get
+            {
+                try
+                {
+                    ISqlMapper mapper = Mapper.Instance();
+                    //mapper.DataSource.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                    return mapper;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+        public static string executeFunction()
+        {
+
+            ISqlMapper mapper = EntityMapper;
+            string str = mapper.QueryForObject<string>("FindPageId", "Footer");
+            return str;
+        }
+
     }
 }
---
+++ OYH.CS/MybatisNET/VO/StudentVO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OYH.CS.MybatisNET.VO
+{
+    public class StudentVO
+    {
+        public string NUMBER { get; set; }
+        public string NAME { get; set; }
+        public string AGE { get; set; }
+        public string SEX { get; set; }
+
+        public string KOREAN { get; set; }
+        public string ENGLISH { get; set; }
+        public string MATH { get; set; }
+        public string SOCIAL { get; set; }
+        public string SCIENCE { get; set; }
+    }
+}
